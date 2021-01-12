eab3ae4018ff0dc9102363b039149ae5791aa5e3
DannyFz <71345818+DannyFz@users.noreply.github.com>
2021-01-11 23:49:58 -0600

    Metodo Delete y Update

--- app/Controllers/Http/DirectorioController.js
+++ app/Controllers/Http/DirectorioController.js
@@ -8,6 +8,7 @@ class DirectorioController {
   async index ({ request}) {
     return await Directorio.all();
   }
+
   //POST
   async store ({ request, response}) {
     const input = request.all()
@@ -28,14 +29,40 @@ class DirectorioController {
     })
   }
 
+  //get
   async show ({ params, request, response}) {
     return await Directorio.findOrFail(params.id)
   }
 
+  //put
   async update ({ params, request, response }) {
+    const input = request.all()
+
+  const validation = await validateAll(input, {
+    
+    'telefono' : 'required|unique:directorios,telefono,id,' + params.id
+  })
+  if(validation.fails()){
+    return validation.messages();
   }
 
-  async destroy ({ params, request, response }) {
+    await Directorio.query().where('id' , params.id).update(input)
+
+    return response.json({
+      rer:true,
+      message: "Se a modificado correctamente"
+    })
+  }
+
+  //delete
+  async destroy ({ params, response}) {
+    const directorio = await Directorio.findOrFail(params.id)
+    await directorio.delete()
+
+    return response.json({
+      rer:true,
+      message: "El registro a sido eliminado"
+    })
   }
 }
 
